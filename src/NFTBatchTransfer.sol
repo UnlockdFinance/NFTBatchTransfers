9f6cc189fd8232d4ffa45137d2653b12e62aa574
fepvenancio <filipeepv@gmail.com>
2023-09-05 17:09:08 +0100

    Gas code improved - 1st tests created

--- src/NFTBatchTransfer.sol
+++ src/NFTBatchTransfer.sol
@@ -1,49 +1,129 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "../../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
 /**
  * @title NFTBatchTransfer
- * @dev A smart contract to transfer multiple NFTs in a single transaction.
+ * @dev This is a public contract in order to allow batch transfers of NFTs,
+ * in a single transaction, reducing gas costs and improving efficiency.
  */
 contract NFTBatchTransfer {
 
-    uint256 public immutable MAX_NFTS = 50;  // maximum number of NFTs that can be transferred in a single transaction
-    
+    // Immutable address for the CryptoPunks contract. This is set at deployment and cannot be changed afterwards.
+    address immutable public punkContract;
+
+    // Struct to encapsulate information about an individual NFT transfer.
+    // It holds the address of the ERC721 contract and the specific token ID to be transferred.
     struct NftTransfer {
         address contractAddress;
         uint256 tokenId;
     }
 
-    // Custom errors
-    error TooManyNfts(uint256 provided, uint256 maxAllowed);
-
-    event TransferBatch(address indexed from, address indexed to, address[] contracts, uint256[] tokenIds);
+    modifier nonZeroAddress(address _address) {
+        require(_address != address(0), "Invalid address");
+        _;
+    }
 
     /**
-     * @dev Transfers multiple NFTs from the caller to a recipient.
-     * @param nftTransfers An array of NftTransfer structs.
-     * @param to The address of the recipient.
+     * @dev Constructor to set the initial state of the contract.
+     * @param _punkContract The address of the CryptoPunks contract.
+     */
+    constructor(address _punkContract) {
+        punkContract = _punkContract;
+    }
+
+    /**
+     * @dev Orchestrates a batch transfer of standard ERC721 NFTs.
+     * @param nftTransfers An array of NftTransfer structs detailing the NFTs to be moved.
+     * @param to The recipient's address.
      */
     function batchTransferFrom(
         NftTransfer[] calldata nftTransfers,
         address to
     ) external {
-        if (nftTransfers.length > MAX_NFTS) {
-            revert TooManyNfts(nftTransfers.length, MAX_NFTS);
+        uint256 length = nftTransfers.length;
+
+        // Capturing the initial gas at the start for later comparisons. Useful to preemptively identify
+        // transactions that might run out of gas and revert them proactively.
+        uint256 gasLeftStart = gasleft();
+
+        // Iterate through each NFT in the array to facilitate the transfer.
+        for(uint i = 0; i < length;) {
+            address contractAddress = nftTransfers[i].contractAddress;
+            uint256 tokenId = nftTransfers[i].tokenId;
+
+            // Generate the signature for the `transferFrom` function of the ERC721 protocol.
+            bytes4 signature = bytes4(keccak256("transferFrom(address,address,uint256)"));
+
+            // Dynamically call the `transferFrom` function on the target ERC721 contract.
+            (bool success, ) = address(uint160(contractAddress))
+                                            .call(abi.encodeWithSelector(signature, msg.sender, to, tokenId));
+
+            // If the transfer fails or consumes over half the starting gas, revert the transaction.
+            if(!success || gasleft() < gasLeftStart/2) {
+                revert("Transfer failed");
+            }
+
+            // Using unchecked block to prevent overflow checks on each loop iteration for efficiency.
+            unchecked {
+                i++;
+            }
         }
+    }
 
-        address[] memory contracts = new address[](nftTransfers.length);
-        uint256[] memory tokenIds = new uint256[](nftTransfers.length);
+    /**
+     * @dev Manages a batch transfer of NFTs, specifically tailored for CryptoPunks alongside other standard ERC721 NFTs.
+     * @param nftTransfers An array of NftTransfer structs specifying the NFTs for transfer.
+     * @param to The destination address for the NFTs.
+     */
+    function batchPunkTransferFrom(NftTransfer[] calldata nftTransfers, address to) external {
+        
+        // Generating the signature for the `transferPunk` function specific to the CryptoPunks contract.
+        bytes4 punkTransferSig = bytes4(keccak256("transferPunk(address,uint256)"));
+        // If not a CryptoPunk, use the standard ERC721 `transferFrom` function.
+        bytes4 erc721TransferSig = bytes4(keccak256("transferFrom(address,address,uint256)"));
 
-        for (uint i = 0; i < nftTransfers.length; i++) {
-            contracts[i] = nftTransfers[i].contractAddress;
-            tokenIds[i] = nftTransfers[i].tokenId;
-            IERC721(nftTransfers[i].contractAddress).safeTransferFrom(msg.sender, to, nftTransfers[i].tokenId);
-        }
+        uint256 length = nftTransfers.length;
+        uint256 gasLeftStart = gasleft();
+        bool success;
 
-        emit TransferBatch(msg.sender, to, contracts, tokenIds);
+        // Processing batch transfers, differentiating between CryptoPunks and other standard ERC721 tokens.
+        for(uint i = 0; i < length; i++) {
+            address contractAddr = nftTransfers[i].contractAddress;
+            uint256 tokenId = nftTransfers[i].tokenId;
+
+            if(contractAddr != punkContract) {
+                (success, ) = contractAddr
+                    .call(
+                        abi.encodeWithSelector(
+                            erc721TransferSig, 
+                            msg.sender, 
+                            to,
+                            tokenId
+                        )  
+                    );
+            }
+            else {
+                // If it's a CryptoPunk, use the specific `transferPunk` function.
+                (success, ) = punkContract.call(
+                    abi.encodeWithSelector(
+                        punkTransferSig,
+                        to,
+                        tokenId 
+                    )
+                );
+            }
+
+            // Check the transfer status and gas consumption, reverting if necessary.
+            if(!success || gasleft() < gasLeftStart/2) {
+                revert("Transfer failed");
+            } 
+        }  
+    }
+
+    // Explicitly reject any Ether sent to the contract
+    fallback() external {
+        revert("Contract does not accept Ether");
     }
 }
-
--- test/NFTBatchTransfer.t.sol
+++ test/NFTBatchTransfer.t.sol
@@ -4,17 +4,40 @@ pragma solidity 0.8.19;
 import "ds-test/test.sol";
 import "../src/NFTBatchTransfer.sol";
 import "./MockERC721.sol";
+import "./MockPunkMarket.sol";
 
 contract TestNFTBatchTransfer is DSTest {
     NFTBatchTransfer public nftBatchTransfer;
     MockERC721 public mockERC721_1;
     MockERC721 public mockERC721_2;
+    MockPunkMarket public mockPunkMarket;
 
     function setUp() public {
-        nftBatchTransfer = new NFTBatchTransfer();
         mockERC721_1 = new MockERC721("Mock Token 1", "MT1");
         mockERC721_2 = new MockERC721("Mock Token 2", "MT2");
+        mockPunkMarket = new MockPunkMarket();
+        mockPunkMarket.allInitialOwnersAssigned();
+        nftBatchTransfer = new NFTBatchTransfer(address(mockPunkMarket));
     }
+
+    function testSingleNFTTransfer() public {
+        // Mint an NFT
+        mockERC721_1.mint(address(this), 1);
+    
+        // Approve the NFTBatchTransfer contract to move the NFT
+        mockERC721_1.approve(address(nftBatchTransfer), 1);
+    
+        // Set up the NFT for transfer
+        NFTBatchTransfer.NftTransfer[] memory nftTransfers = new NFTBatchTransfer.NftTransfer[](1);
+        nftTransfers[0] = NFTBatchTransfer.NftTransfer(address(mockERC721_1), 1);
+    
+        // Transfer the NFT
+        nftBatchTransfer.batchTransferFrom(nftTransfers, address(0x123));
+    
+        // Check the new owner
+        assertEq(mockERC721_1.ownerOf(1), address(0x123));
+    }
+    
 
     function testBatchTransferFrom() public {
         // Mint some NFTs
@@ -40,5 +63,31 @@ contract TestNFTBatchTransfer is DSTest {
         assertEq(mockERC721_1.ownerOf(1), address(0x123));
         assertEq(mockERC721_1.ownerOf(2), address(0x123));
         assertEq(mockERC721_2.ownerOf(3), address(0x123));
+    }
+
+    function testBatchPunkTransferFrom() public {
+        // Mint some NFTs
+        mockERC721_1.mint(address(this), 1);
+        mockERC721_2.mint(address(this), 2);
+        mockPunkMarket.getPunk(1);
+        mockPunkMarket.transferPunk(address(nftBatchTransfer), 1);
+
+        // Approve the NFTBatchTransfer contract to move the NFTs
+        mockERC721_1.approve(address(nftBatchTransfer), 1);
+        mockERC721_2.approve(address(nftBatchTransfer), 2);
+
+        // Set up the NFTs for transfer
+        NFTBatchTransfer.NftTransfer[] memory nftTransfers = new NFTBatchTransfer.NftTransfer[](3);
+        nftTransfers[0] = NFTBatchTransfer.NftTransfer(address(mockERC721_1), 1);
+        nftTransfers[1] = NFTBatchTransfer.NftTransfer(address(mockERC721_2), 2);
+        nftTransfers[2] = NFTBatchTransfer.NftTransfer(address(mockPunkMarket), 1);
+
+        // Transfer the NFTs
+        nftBatchTransfer.batchPunkTransferFrom(nftTransfers, address(0x123));
+
+        // Check the new owners
+        assertEq(mockERC721_1.ownerOf(1), address(0x123));
+        assertEq(mockERC721_2.ownerOf(2), address(0x123));
+        assertEq(mockPunkMarket.punkIndexToAddress(1), address(0x123));
     }
 }
