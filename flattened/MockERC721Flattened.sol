f0f4cf242218d4ac213764921e97e84bcdabb851
fepvenancio <filipeepv@gmail.com>
2023-09-07 16:13:49 +0100

    update: changes on mockERC721 to match unlockd default

--- flattened/MockERC721Flattened.sol
+++ flattened/MockERC721Flattened.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.8.19;
 
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC721/ERC721.sol)
@@ -1403,104 +1403,231 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     }
 }
 
-// OpenZeppelin Contracts (last updated v4.9.0) (access/Ownable.sol)
+// OpenZeppelin Contracts (last updated v4.8.0) (token/ERC721/extensions/ERC721Enumerable.sol)
+
+// OpenZeppelin Contracts (last updated v4.5.0) (token/ERC721/extensions/IERC721Enumerable.sol)
 
 /**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
+ * @title ERC-721 Non-Fungible Token Standard, optional enumeration extension
+ * @dev See https://eips.ethereum.org/EIPS/eip-721
  */
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+interface IERC721Enumerable is IERC721 {
+    /**
+     * @dev Returns the total amount of tokens stored by the contract.
+     */
+    function totalSupply() external view returns (uint256);
 
     /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
+     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
+     * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
      */
-    constructor() {
-        _transferOwnership(_msgSender());
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+
+    /**
+     * @dev Returns a token ID at a given `index` of all the tokens stored by the contract.
+     * Use along with {totalSupply} to enumerate all tokens.
+     */
+    function tokenByIndex(uint256 index) external view returns (uint256);
+}
+
+/**
+ * @dev This implements an optional extension of {ERC721} defined in the EIP that adds
+ * enumerability of all the token ids in the contract as well as all token ids owned by each
+ * account.
+ */
+abstract contract ERC721Enumerable is ERC721, IERC721Enumerable {
+    // Mapping from owner to list of owned token IDs
+    mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
+
+    // Mapping from token ID to index of the owner tokens list
+    mapping(uint256 => uint256) private _ownedTokensIndex;
+
+    // Array with all token ids, used for enumeration
+    uint256[] private _allTokens;
+
+    // Mapping from token id to position in the allTokens array
+    mapping(uint256 => uint256) private _allTokensIndex;
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC721) returns (bool) {
+        return interfaceId == type(IERC721Enumerable).interfaceId || super.supportsInterface(interfaceId);
     }
 
     /**
-     * @dev Throws if called by any account other than the owner.
+     * @dev See {IERC721Enumerable-tokenOfOwnerByIndex}.
      */
-    modifier onlyOwner() {
-        _checkOwner();
-        _;
+    function tokenOfOwnerByIndex(address owner, uint256 index) public view virtual override returns (uint256) {
+        require(index < ERC721.balanceOf(owner), "ERC721Enumerable: owner index out of bounds");
+        return _ownedTokens[owner][index];
     }
 
     /**
-     * @dev Returns the address of the current owner.
+     * @dev See {IERC721Enumerable-totalSupply}.
      */
-    function owner() public view virtual returns (address) {
-        return _owner;
+    function totalSupply() public view virtual override returns (uint256) {
+        return _allTokens.length;
     }
 
     /**
-     * @dev Throws if the sender is not the owner.
+     * @dev See {IERC721Enumerable-tokenByIndex}.
      */
-    function _checkOwner() internal view virtual {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    function tokenByIndex(uint256 index) public view virtual override returns (uint256) {
+        require(index < ERC721Enumerable.totalSupply(), "ERC721Enumerable: global index out of bounds");
+        return _allTokens[index];
     }
 
     /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby disabling any functionality that is only available to the owner.
+     * @dev See {ERC721-_beforeTokenTransfer}.
      */
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+
+        if (batchSize > 1) {
+            // Will only trigger during construction. Batch transferring (minting) is not available afterwards.
+            revert("ERC721Enumerable: consecutive transfers not supported");
+        }
+
+        uint256 tokenId = firstTokenId;
+
+        if (from == address(0)) {
+            _addTokenToAllTokensEnumeration(tokenId);
+        } else if (from != to) {
+            _removeTokenFromOwnerEnumeration(from, tokenId);
+        }
+        if (to == address(0)) {
+            _removeTokenFromAllTokensEnumeration(tokenId);
+        } else if (to != from) {
+            _addTokenToOwnerEnumeration(to, tokenId);
+        }
     }
 
     /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
+     * @dev Private function to add a token to this extension's ownership-tracking data structures.
+     * @param to address representing the new owner of the given token ID
+     * @param tokenId uint256 ID of the token to be added to the tokens list of the given address
      */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _transferOwnership(newOwner);
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        uint256 length = ERC721.balanceOf(to);
+        _ownedTokens[to][length] = tokenId;
+        _ownedTokensIndex[tokenId] = length;
     }
 
     /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction.
+     * @dev Private function to add a token to this extension's token tracking data structures.
+     * @param tokenId uint256 ID of the token to be added to the tokens list
      */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
+    function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    }
+
+    /**
+     * @dev Private function to remove a token from this extension's ownership-tracking data structures. Note that
+     * while the token is not assigned a new owner, the `_ownedTokensIndex` mapping is _not_ updated: this allows for
+     * gas optimizations e.g. when performing a transfer operation (avoiding double writes).
+     * This has O(1) time complexity, but alters the order of the _ownedTokens array.
+     * @param from address representing the previous owner of the given token ID
+     * @param tokenId uint256 ID of the token to be removed from the tokens list of the given address
+     */
+    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private {
+        // To prevent a gap in from's tokens array, we store the last token in the index of the token to delete, and
+        // then delete the last slot (swap and pop).
+
+        uint256 lastTokenIndex = ERC721.balanceOf(from) - 1;
+        uint256 tokenIndex = _ownedTokensIndex[tokenId];
+
+        // When the token to delete is the last token, the swap operation is unnecessary
+        if (tokenIndex != lastTokenIndex) {
+            uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
+
+            _ownedTokens[from][tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+            _ownedTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+        }
+
+        // This also deletes the contents at the last position of the array
+        delete _ownedTokensIndex[tokenId];
+        delete _ownedTokens[from][lastTokenIndex];
+    }
+
+    /**
+     * @dev Private function to remove a token from this extension's token tracking data structures.
+     * This has O(1) time complexity, but alters the order of the _allTokens array.
+     * @param tokenId uint256 ID of the token to be removed from the tokens list
+     */
+    function _removeTokenFromAllTokensEnumeration(uint256 tokenId) private {
+        // To prevent a gap in the tokens array, we store the last token in the index of the token to delete, and
+        // then delete the last slot (swap and pop).
+
+        uint256 lastTokenIndex = _allTokens.length - 1;
+        uint256 tokenIndex = _allTokensIndex[tokenId];
+
+        // When the token to delete is the last token, the swap operation is unnecessary. However, since this occurs so
+        // rarely (when the last minted token is burnt) that we still do the swap here to avoid the gas cost of adding
+        // an 'if' statement (like in _removeTokenFromOwnerEnumeration)
+        uint256 lastTokenId = _allTokens[lastTokenIndex];
+
+        _allTokens[tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+        _allTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+
+        // This also deletes the contents at the last position of the array
+        delete _allTokensIndex[tokenId];
+        _allTokens.pop();
     }
 }
 
-contract MockERC721 is ERC721, Ownable {
+/**
+ * @title MintableERC721
+ * @dev ERC721 minting logic
+ */
+contract CustomERC721 is ERC721Enumerable {
+  error ZeroAddress();
+  error MaxSupplyReached();
+  error MaxNFTsPerWalletReached();
 
-    string private _mockBaseURI;
+  uint16 public _currTokenId = 1;
+  string public baseURI;
+  address private owner;
+  uint256 public constant MAX_SUPPLY = 10_000;
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+  constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+    baseURI = "";
+    owner = _msgSender();
+  }
 
-    function mint(address to, uint256 tokenId) external {
-        _mint(to, tokenId);
+  /**
+   * @dev Function to mint tokens
+   * @param to The address receiving the NFT
+   * @param amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address to, uint256 amount) public returns (bool) {
+    if ((balanceOf(msg.sender) + amount) >= 3) revert MaxNFTsPerWalletReached();
+    if (to == address(0)) revert ZeroAddress();
+    if (_currTokenId + amount >= MAX_SUPPLY) revert MaxSupplyReached();
+
+    for (uint256 i; i < amount; ) {
+      _mint(to, _currTokenId);
+      unchecked {
+        ++i;
+        _currTokenId = _currTokenId + 1;
+      }
     }
+    return true;
+  }
 
-    function setBaseURI(string calldata baseURI_)
-        external
-        onlyOwner
-    {
-        _mockBaseURI = baseURI_;
-    }
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
 
-    function _baseURI() internal view override returns (string memory) {
-        return _mockBaseURI;
-    }
+  function setBaseURI(string memory baseURI_) public {
+    baseURI = baseURI_;
+  }
 }
 
--- test/mocks/MockERC721.sol
+++ test/mocks/MockERC721.sol
@@ -1,27 +1,54 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.8.19;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {ERC721} from "../../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "../../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract MockERC721 is ERC721, Ownable {
+/**
+ * @title MintableERC721
+ * @dev ERC721 minting logic
+ */
+contract MockERC721 is ERC721Enumerable {
+  error ZeroAddress();
+  error MaxSupplyReached();
+  error MaxNFTsPerWalletReached();
 
-    string private _mockBaseURI;
+  uint16 public _currTokenId = 1;
+  string public baseURI;
+  address private owner;
+  uint256 public constant MAX_SUPPLY = 10_000;
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+  constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+    baseURI = "";
+    owner = _msgSender();
+  }
 
-    function mint(address to, uint256 tokenId) external {
-        _mint(to, tokenId);
+  /**
+   * @dev Function to mint tokens
+   * @param to The address receiving the NFT
+   * @param amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address to, uint256 amount) public returns (bool) {
+    if ((balanceOf(msg.sender) + amount) >= 3) revert MaxNFTsPerWalletReached();
+    if (to == address(0)) revert ZeroAddress();
+    if (_currTokenId + amount >= MAX_SUPPLY) revert MaxSupplyReached();
+
+    for (uint256 i; i < amount; ) {
+      _mint(to, _currTokenId);
+      unchecked {
+        ++i;
+        _currTokenId = _currTokenId + 1;
+      }
     }
+    return true;
+  }
 
-    function setBaseURI(string calldata baseURI_)
-        external
-        onlyOwner
-    {
-        _mockBaseURI = baseURI_;
-    }
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
 
-    function _baseURI() internal view override returns (string memory) {
-        return _mockBaseURI;
-    }
+  function setBaseURI(string memory baseURI_) public {
+    baseURI = baseURI_;
+  }
 }
